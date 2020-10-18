a4d1655c6770c7f73b3ed9dffa2fdfb5998daa58
Naufal Aji Wibowo <naufal.aji@mii.co.id>
2020-10-18 19:08:29 +0700

    add module Penutupan

--- EPOLIS/Controllers/PenutupansController.cs
+++ EPOLIS/Controllers/PenutupansController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using EPOLIS.Context;
 using EPOLIS.Models;
 using EPOLIS.Repositories.Data;
+using System.Web.Http.Results;
 
 namespace EPOLIS.Controllers
 {
@@ -23,9 +24,52 @@ namespace EPOLIS.Controllers
         // GET: Penutupans
         public async Task<ActionResult<Penutupan>> Index()
         {
-            return View(await _penutupanRepository.Get());
+            return View(await LoadData());
         }
 
-        
+        public async Task<List<Penutupan>> LoadData()
+        {
+            var result = await _penutupanRepository.Get();
+            return result;
+        }
+
+        public async Task<ActionResult<Penutupan>> Get(int id)
+        {
+            var result = await _penutupanRepository.Get(id);
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Penutupan>> Post(Penutupan penutupan)
+        {
+            var result = await _penutupanRepository.Post(penutupan);
+            if (!result.Equals(0))
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Penutupan>> Put(Penutupan penutupan)
+        {
+            var result = await _penutupanRepository.Put(penutupan);
+            if (!result.Equals(0))
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Penutupan>> Delete(int id)
+        {
+            var result = await _penutupanRepository.Delete(id);
+            if (!result.Equals(0))
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
     }
 }
