6a6958d25e2fb91de72f813d12a8e93c10747257
bowo <rooms.wibowo3@gmail.com>
2020-10-24 14:15:19 +0700

    add

---
+++ EPOLIS/wwwroot/template/js/IntegrationHelper.js
@@ -0,0 +1,34 @@
+﻿function invokeWebServices(ServicesName, methodName, parameter) {
+    debugger;
+    var result;
+    var dataPost = ''
+    if (parameter != null) {
+        dataPost = JSON.stringify(parameter);
+    }
+    //JSON.stringify(parameter);  
+
+    $.ajax({
+        cache: false,
+        async: false,
+        url: '../' + ServicesName + '/' + methodName,
+        data: dataPost,
+        dataType: "json",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: successFunc,
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            debugger
+            //w2alert(errorThrown)
+        }
+    });
+
+    function successFunc(data, status) {
+        debugger;
+        result = data;
+
+
+    }
+
+
+    return result;
+}
