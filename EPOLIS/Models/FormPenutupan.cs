84404f99851cc72a56a90500841fc4055c6c3388
bowo <rooms.wibowo3@gmail.com>
2020-10-24 14:03:42 +0700

    add css

--- EPOLIS/Models/FormPenutupan.cs
+++ EPOLIS/Models/FormPenutupan.cs
@@ -9,6 +9,7 @@ namespace EPOLIS.Models
 {
     public class FormPenutupan : IEntity
     {
+        public int tnasabahID { get; set; }
         //[Display(Name = "JENISPENUTUPAN")]
         public string JENISPENUTUPAN { get; set; }
         //[Display(Name = "NOREGPENUTUPAN")]
@@ -16,31 +17,31 @@ namespace EPOLIS.Models
         public string NOREGPENUTUPAN { get; set; }
         //[Display(Name = "Admin Id")]
         //[StringLength(20, MinimumLength = 1)]
-        public string NOFASILITAS { get; set; }
+        public string ADMINID { get; set; }
         //[Display(Name = "Tanggal Input")]
         //[DataType(DataType.Date)]
-        public DateTime NOREKENING { get; set; }
+        public DateTime TGLINPUT { get; set; }
         //[Display(Name = "Tanggal Otorisasi")]
         //[DataType(DataType.Date)]
-        public DateTime NONASABAH { get; set; }
+        public DateTime TGLOTORISASI { get; set; }
         //[Display(Name = "Status")]
         //[StringLength(2, MinimumLength = 2)]
         public string STATUS { get; set; }
         //[Display(Name = "Nama")]
         //[StringLength(40, MinimumLength = 1)]
-        public string NAMA { get; set; }
+        public string NOSKK { get; set; }
         //[Display(Name = "NOSKK")]
         //[StringLength(1, MinimumLength = 1)]
-        public string NOSKK { get; set; }
+        public string NOPK { get; set; }
         //[Display(Name = "No. SKK")]
         //[DataType(DataType.Date)]
-        public DateTime ALAMATNASABAH { get; set; }
+        public string CIF { get; set; }
         //[Display(Name = "Tanggal SKK")]
         //[StringLength(50, MinimumLength = 1)]
-        public string TELPRUMAH { get; set; }
+        public  ISBROKER { get; set; }
         //[Display(Name = "No. PK")]
         //[StringLength(20, MinimumLength = 1)]
-        public string PEKERJAAN { get; set; }
+        public string PEKE { get; set; }
         //[Display(Name = "CIF")]
         //[StringLength(20, MinimumLength = 1)]
         public int NOHP { get; set; }
